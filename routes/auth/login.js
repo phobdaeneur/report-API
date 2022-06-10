9868fa91c0ba26bfcc209b94f600d8ceff69ec2d
nathapon <hellonathapon@gmail.com>
2022-06-10 14:42:35 +0700

    Hash MD5 and compare it to db pwd

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,16 +1,40 @@
 const express = require("express");
 const router = express.Router();
-const { pool_zb_log } = require("../../config/db");
+const { pool_zb } = require("../../config/db");
+const ApiError = require("../../error/ApiError");
+const md5 = require("md5");
 
 /* POST login */
-router.get("/", async (req, res, next) => {
-  /**
-   * TODO: get username unhash password and compare
-   */
+router.post("/", async (req, res, next) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  if (!username || !password) {
+    next(ApiError.badRequest("Invalid username or password!"));
+  }
   try {
-    const pool_zb_logq = await pool_zb_log();
-    const result = await pool_zb_logq.query(``);
-    console.log(result);
+    const pool_zbq = await pool_zb();
+    const user = await pool_zbq.query(`
+    SELECT *
+    FROM [ZebraDB].[dbo].[staff]
+    where login_name = '${username}'
+    `);
+
+    if (!user.recordset.length) {
+      res.status(401).json("Invalid username!");
+    }
+
+    /**
+     * * hash MD5 and compare with retrieved hash
+     */
+    const hashMD5 = md5(password.toString()).toUpperCase();
+    if (user.recordset[0].login_pwd.replace(/-/g, "") !== hashMD5) {
+      res.status(401).json("Invalid password!");
+    }
+    res.status(200).json("Successfully Authenticated! :)");
+
+    /**
+     * * set JWT in cookie for authentication
+     */
   } catch (err) {
     next(err);
   }
