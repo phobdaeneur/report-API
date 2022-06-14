1800431da91e942376abb00bcf79e7b530b7696c
nathapon <hellonathapon@gmail.com>
2022-06-14 16:35:38 +0700

    Add JWT access token

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 // const { poolPromise } = require("../../config/db");
-const { pool_zb, pool_zb_log } = require("../../config/db");
+const { pool_zb } = require("../../config/db");
 const ApiError = require("../../error/ApiError");
+const isAuth = require("../../middlewares/isAuth");
 
-/* GET api/? */
-router.get("/fleets", async (req, res, next) => {
-  const { fleetName } = req.body;
+/* GET api/fleets */
+router.post("/fleets", isAuth, async (req, res, next) => {
+  const { loginName } = req.body;
 
   try {
     const pool_zbq = await pool_zb();
@@ -16,17 +17,17 @@ router.get("/fleets", async (req, res, next) => {
       FROM [ZebraDB].[dbo].[staff] a
       left join [ZebraDB].[dbo].[profile_fleet] b on a.profile_id = b.profile_id
       left join [ZebraDB].[dbo].[fleet] c on b.fleet_id = c.fleet_id
-      where login_name = 'Toe'
+      where login_name = '${loginName}'
     `);
 
-    console.log(result);
     res.status(200).json(result.recordset);
   } catch (err) {
     next(err);
   }
 });
 
-router.post("/fleet/vehicles", async (req, res, next) => {
+/* POST api/fleet/vehicles */
+router.post("/fleet/vehicles", isAuth, async (req, res, next) => {
   const { fleetId } = req.body;
 
   if (!fleetId) {
@@ -35,7 +36,6 @@ router.post("/fleet/vehicles", async (req, res, next) => {
   } else {
     try {
       const pool_zbq = await pool_zb();
-      const pool_zb_logq = await pool_zb_log();
 
       const result = await pool_zbq.query(`
       SELECT a.fleet_id
@@ -49,23 +49,28 @@ router.post("/fleet/vehicles", async (req, res, next) => {
       ,c.namt
       ,b.distance
       ,e.evt_id
+      ,ISNULL(ZebraDB.dbo.IsEngineOnTheLastByTime_Log(a.veh_id ,c.local_timestamp),0)
       ,case 
       when isnull(e.evt_id,12) = 2 then 'SPEEDING'
       when isnull(e.evt_id,12) = 3 then 'IDLE'
-      when isnull(e.evt_id,12) = 23 or ISNULL(ZebraDB.dbo.IsEngineOnTheLastByTime_Log(a.veh_id ,c.local_timestamp),0) = 0 then 'ENGINE OFF'
-      when e.evt_id not in (2,3,23) and isnull(c.local_timestamp,GETDATE()-30) < GETDATE()-7 then 'OFFLINE'
-      --when (e.evt_id is null or e.evt_id not in (2,3,23)) and isnull(c.local_timestamp,GETDATE()-30) > GETDATE()-7 then 'NORMAL'
+      when isnull(c.local_timestamp,GETDATE()-30) between (dateadd(hour,-24,getdate())) and (dateadd(hour,-12,getdate())) then 'NO SIGNAL 12Hr.'
+      when isnull(c.local_timestamp,GETDATE()-30) < (dateadd(hour,-24,getdate())) then 'NO SIGNAL 24Hr.'
+      when isnull(e.evt_id,12) not in (2,3) 
+        and isnull(c.local_timestamp,GETDATE()-30) > (dateadd(hour,-12,getdate())) 
+        and ISNULL(ZebraDB.dbo.IsEngineOnTheLastByTime_Log(a.veh_id ,c.local_timestamp),0) = 0 then 'ENGINE OFF'
       else 'NORMAL'
       end as Status
+      ,case when f.tag_msg is null then  '?' else  CAST(REPLACE( dbo.GetLastTemperature_LogCur_New(a.veh_id,0,c.local_timestamp) , '999.99','-') AS varchar) end AS Temp1
+      ,case when f.tag_msg is null then  '?' else  CAST(REPLACE( dbo.GetLastTemperature_LogCur_New(a.veh_id,1,c.local_timestamp) , '999.99','-') AS varchar) end AS Temp2
         FROM  [ZebraDB].[dbo].[fleet_vehicle] a
         left join [ZebraDB_Log].[dbo].[veh_current_location] b on a.veh_id =b.veh_id
         left join [ZebraDB_Log].[dbo].[log_msg] c on b.ref_idx = c.idx
         left join [ZebraDB].[dbo].[vehicle] d on a.veh_id = d.veh_id
         Left join [ZebraDB_Log].[dbo].[veh_current_event] e on c.idx = e.ref_idx
+        left join [ZebraDB_Log].[dbo].[log_msg_tag] f on c.idx = f.ref_idx
         where a.fleet_id = ${parseInt(fleetId)}
     `);
 
-      console.log(result);
       res.status(200).json(result.recordset);
     } catch (err) {
       next(err);
